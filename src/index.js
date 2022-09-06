688be101ba30f6559d9abfac083d45ecb76032dd
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-09-06 16:06:34 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-import './index.css'
+import './index.css';
+
+const display = document.querySelector('.movie-api');
+const fetchmovie = async () => {
+  for (let i = 1; i < 10; i += 1) {
+    await getMovie(i);
+  }
+};
+const getMovie = async (id) => {
+  const urlHistory = `https://api.tvmaze.com/shows/${id}`;
+  const res = await fetch(urlHistory);
+  const movie = await res.json();
+  createMovielist(movie);
+};
+fetchmovie();
+function createMovielist(movie) {
+  const movieEl = document.createElement('div');
+  movieEl.classList.add('movie');
+  const movies = `
+  <div class="movieli">
+  <h2 class="name">${movie.name}</h2>
+  <img src="${movie.image.medium}" alt="img">
+  <button type="button" class="comments-btn1">Comments</button></div>
+  </div>
+  `;
+  movieEl.innerHTML = movies;
+  display.appendChild(movieEl);
+}
