cc44a8f578f149c04fb9dc13818935e72690f14b
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-09-09 12:05:03 +0200

    Update displayPopup.js

--- src/modules/displayPopup.js
+++ src/modules/displayPopup.js
@@ -2,20 +2,23 @@ import fromAPI from './fromAPI.js';
 import getUrl from './getUrl.js';
 
 const container = document.querySelector('.container');
-
+const navB = document.querySelector('.nav');
 const displayPopup = (i) => {
   const url = `${getUrl()}/comments?item_id=movie${i}`;
   const popUp = document.getElementById(`popup-${i}`);
   const commentList = document.getElementById(`comments${i}`);
   (async () => {
     const comments = await fromAPI(url);
-    commentList.innerHTML = `Comments(${comments.length ? comments.length : 0})`;
+    commentList.innerHTML = `Comments(${
+      comments.length ? comments.length : 0
+    })`;
     comments.forEach((comment) => {
       commentList.innerHTML += `<li class="comment-list">${comment.creation_date} ${comment.username}: ${comment.comment}</li>`;
     });
   })();
   popUp.style.display = 'block';
   container.style.display = 'none';
+  navB.style.display = 'none';
   window.scrollTo(0, 0);
 };
 
