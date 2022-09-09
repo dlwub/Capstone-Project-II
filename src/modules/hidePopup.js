3cfc4b7bf60bb3a9a4a7447c4709bcde9771638b
X-Elie-X <103565978+X-Elie-X@users.noreply.github.com>
2022-09-09 12:14:20 +0200

    Update hidePopup.js

--- src/modules/hidePopup.js
+++ src/modules/hidePopup.js
@@ -1,9 +1,10 @@
 const container = document.querySelector('.container');
-
+const navB = document.querySelector('.nav');
 const hidePopup = (e) => {
   const popUp = document.getElementById(`popup-${e.target.id}`);
   container.style.display = 'block';
   popUp.style.display = 'none';
+  navB.style.display = 'block';
 };
 
 export default hidePopup;
